a6addefafce247dd97c3219257061ed72191fa60
Ranjan <ranjannp802114@gmail.com>
2024-10-31 03:19:58 +0530

    update 2.0

--- 119_Pascal's Triangle_II.cpp
+++ 119_Pascal's Triangle_II.cpp
@@ -2,15 +2,12 @@
 #include<vector>
 
 using namespace std;
-// Time Complexity -> O(n^2) 
+// Time Complexity -> O(n) 
 // Space Complexity -> O(n)
 vector<int> getRow(int rowIndex) {
-    vector<int> pasRow;
-    for( int i=0 ; i <= rowIndex ; i++){
-        pasRow.push_back( 1 );
-        for( int j=i-1 ; j > 0 ; j-- ){
-            pasRow[j] += pasRow[j-1] ;
-        }
+    vector<int> pasRow( rowIndex+1 , 1 );
+    for( int i=1 ; i < rowIndex ; i++){
+        pasRow[i] = (long) pasRow[i-1] * (rowIndex - i + 1) / i ;
     }
     return pasRow ;
 }
