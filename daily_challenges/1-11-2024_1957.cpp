c276ee50734712edf39dd8e811307413903a2a66
Ranjan <ranjannp802114@gmail.com>
2024-11-02 03:20:49 +0530

    updated 1.0

--- daily_challenges/1-11-2024_1957.cpp
+++ daily_challenges/1-11-2024_1957.cpp
@@ -4,20 +4,17 @@
 using namespace std;
 
 string makeFancyString(string s) {
-    int n = s.length();
-    string temp;
-    temp.push_back( s[0] );
-    for( int i=1 , c=1 , k=1 ; i < n ; i++){
-        if( temp[i-k]!=s[i] ){
-            temp.push_back( s[i] );
-            c=1;
+    string t;
+    int cnt =0;
+    for( auto ch : s ){
+        cnt++;
+        if( !t.empty() && t.back()!=ch ){
+            cnt=1;
         }
-        else if( c<2 && temp[i-k]==s[i] ){
-            temp.push_back( s[i] );
-            c++;
+        if( cnt < 3 ){
+            t.push_back( ch );
         }
-        else
-            k++;
     }
-    return temp;
+    
+    return t;
 }
