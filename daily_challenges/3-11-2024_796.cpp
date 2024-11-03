37c950e0198da6c5fd456c1bd96aaed4939dbdab
Ranjan <ranjannp802114@gmail.com>
2024-11-04 02:01:04 +0530

    up-2.0 Optimize Sol

--- daily_challenges/3-11-2024_796.cpp
+++ daily_challenges/3-11-2024_796.cpp
@@ -5,10 +5,15 @@ using namespace std;
 
 bool rotateString(string s, string goal) {
     int l = s.length();
-    for( int i=0 ; i < l ; i++ ){
-        rotate( s.begin() , s.begin()+1 , s.end() );
-        if( s == goal )
-            return true;
+    int n = goal.length();
+    // T.C -> O(n)
+    // S.C -> O(1)
+    if( l != n )
+        return false;
+    if( (s+s).find(goal) != string::npos ){
+        return true;
     }
+    //(s+s) concatenate
+    //If you concatenate a string with itself , it will contain all rotations as a substring in it...
     return false;
 }
