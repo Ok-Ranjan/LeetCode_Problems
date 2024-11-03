3c4386a4376f715509bebed70ed0e198c246e6a8
Ranjan <ranjannp802114@gmail.com>
2024-11-04 01:13:51 +0530

    up-1.1 using rotate(),Brute Force

--- daily_challenges/3-11-2024_796.cpp
+++ daily_challenges/3-11-2024_796.cpp
@@ -1,20 +1,14 @@
 #include<iostream>
 #include<string>
-
+#include<algorithm>
 using namespace std;
 
 bool rotateString(string s, string goal) {
     int l = s.length();
-    int i=0;
-    while( i < l ){
-        int temp = s[0];
-        for( int j=1; j < l ; j++){
-            s[j-1] = s[j];
-        }
-        s[l-1] = temp;
+    for( int i=0 ; i < l ; i++ ){
+        rotate( s.begin() , s.begin()+1 , s.end() );
         if( s == goal )
             return true;
-        i++;
     }
     return false;
 }
