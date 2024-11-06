ea71189298a6e79a7322ffb03466337a31e0ec1d
Ranjan <ranjannp802114@gmail.com>
2024-11-07 01:50:36 +0530

    Approach-2 (Using Simple Segment sorting check)

--- daily_challenges/6-11-2024_3011.cpp
+++ daily_challenges/6-11-2024_3011.cpp
@@ -5,23 +5,41 @@ using namespace std;
 
 int setOfBits( int n ); //decleration
 
+//Approach-2 (Using simple segment sorting check)
+//T.C : O(n)
+//S.C : O(1)
 bool canSortArray(vector<int>& nums) {
     int n = nums.size();
-    //Bubble Sort
-    for( int i=0 ; i < n ; i++ ){ //total no. of round
-        bool swapped  = false;    
-        for( int j = 0 ; j < n-i-1 ; j++){ 
-            if( nums[j] > nums[j+1] && setOfBits(nums[j]) != setOfBits(nums[j+1]))
-                return false; //setOfBits not equal 
-            else if( nums[j] > nums[j+1] ){
-                swap( nums[j] , nums[j+1] );
-                swapped = true;
-            }
+
+    int numOfSetBits = setOfBits(nums[0]);
+    int minOfSegment = nums[0];
+    int maxOfSegment = nums[0];
+
+    int maxOfPreSegment = INT_MIN;
+
+    for( int i=1 ; i < n ; i++){
+        if( numOfSetBits == setOfBits(nums[i]) ){   //they belong to same segment's
+            minOfSegment = min( minOfSegment , nums[i]);    //find min of current segment
+            maxOfSegment = max( maxOfSegment , nums[i]);    //find max of current segment
+            continue;
+        } else{ //Element belongs to new segment's
+
+            if( maxOfPreSegment > minOfSegment) //not possible to sorted
+                return false;
+            
+            //update the previous Segment's max 
+            maxOfPreSegment = maxOfSegment;
+
+            //new segment started now
+            minOfSegment = nums[i];
+            maxOfSegment = nums[i];
+            numOfSetBits = setOfBits(nums[i]);
         }
-        if( !swapped )
-            break;  //nums is sorted
     }
-    return true; //sorted
+    //final check
+    if( maxOfPreSegment > minOfSegment )
+        return false;
+    return true;
 }
 
 // set Bits count
